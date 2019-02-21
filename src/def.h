bdae3819591ba3335395fa2774a4884e5f06a601
Roland4444 <turbopollux@rambler.ru>
2019-02-21 11:46:15 +0400

    refactor

--- src/def.h
+++ src/def.h
@@ -25,19 +25,16 @@ typedef struct
   uint64_t size;
 }   ContentInfo;
 
-typedef int (*v_create_session)(struct Session*  , char *);
 typedef int (*create_session)(struct Session*  , char *);
 typedef int (*BKKCheck)(Session *,  uint8_t * , uint64_t);
-typedef int (*BKK)(Session *,  uint8_t * , uint64_t);
-
 
 typedef struct
 {
   void(*Checker)(void);    
   void * handles[2];
   Session * sessions[2];
-  BKK v_check;
-  BKK i_check;
+  BKKCheck v_check;
+  BKKCheck i_check;
   ContentInfo*(*loadContent)(char * filename);
   void (*initSessions)(struct Checker*);
   Session * (*initSession)(void*, char *, char *);
