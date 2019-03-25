2f7a538276ae7a79c03e63bbae94063acfb3e2df
Roland4444 <turbopollux@rambler.ru>
2019-03-25 09:56:43 +0400

    update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc src/main.c -Isrc -ldl -o obj/main; gcc -g -fPIC -ldl -Isrc src/main.c -shared -o obj/libnosess.so
+gcc -w src/main.c -Isrc -ldl -o obj/main; gcc -w -g -fPIC -ldl -Isrc src/main.c -shared -o obj/libnosess.so
